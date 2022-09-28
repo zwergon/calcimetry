ae9f27c2684f3f9326205ac3a476f6695b06e3f1
lecomtje <jean-francois.lecomte@ifpen.fr>
2022-09-28 10:31:02 +0200

    adust pipeline

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     name='Calcimetry',
     version='0.1',
     description='module pour gérér les données Calcimetries de l andra',
-    author='Jef Lecomte, John Armitage, Renaud Divies',
+    author='Jef Lecomte, John Armitage, Renaud Divies, Nina Khvoenkova',
     author_email='jean-francois.lecomte@ifpen.fr',
+    url='https://tellus.gitlabpages.ifpen.fr/andra/ai.calcimetry/CalcimetryAPI.html',
+    packages=['calcimetry', 'database', 'tests']
     )
